221f4e37c1f9ba88fa9664d5be43600452116143
Zhang Jin <zhangjin@algorix.co>
2025-03-31 18:00:37 +0800

    learn extend httpserver

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # go-examples
+* [参考1](https://golang.xiniushu.com/guide.html)
+* [参考2](https://geektutu.com/post/gee-day1.html)
---
+++ basic/httpserver_demo.go
@@ -0,0 +1,16 @@
+package basic
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func StartSimpleHttpServer() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
